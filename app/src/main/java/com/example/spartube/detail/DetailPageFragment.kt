5bf87f8c3be001f1f8ae644e234d8a309af9da76
Martin Kwon <mindori9097@gmail.com>
2023-09-26 22:46:53 +0900

    [FEAT] : ⚙️: DetailPageFragment 바인딩

--- app/src/main/java/com/example/spartube/detail/DetailPageFragment.kt
+++ app/src/main/java/com/example/spartube/detail/DetailPageFragment.kt
@@ -6,8 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.spartube.R
+import com.example.spartube.databinding.FragmentDetailPageBinding
 
 class DetailPageFragment : Fragment() {
+    private lateinit var binding: FragmentDetailPageBinding
+    private var _binding: FragmentDetailPageBinding? = null
 
     companion object{
 
@@ -22,5 +25,8 @@ class DetailPageFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail_page, container, false)
     }
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
