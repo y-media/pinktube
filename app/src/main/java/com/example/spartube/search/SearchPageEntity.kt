7630c2fb1d63ff390332c1bf96566a5431f1ceda
developShin <blueskyroad@naver.com>
2023-10-03 15:14:18 +0900

    [FEAT] : ⚙️(sd): 검색 했을때 나오는 나오는 화면 클릭시 detailpage로 이동 edittext 키보드 내리기

--- app/src/main/java/com/example/spartube/search/SearchPageEntity.kt
+++ app/src/main/java/com/example/spartube/search/SearchPageEntity.kt
@@ -1,9 +1,15 @@
 package com.example.spartube.search
 
+import com.example.spartube.home.BindingModel
+
 data class SearchPageEntity(
     val title: String,
     val videoId: String,
     val description: String,
     val publishedAt: String,
     val thumbnails: String
-)
+) {
+    fun toBindingModel() = BindingModel(
+        videoId, title, thumbnails, null, "", publishedAt, description
+    )
+}
